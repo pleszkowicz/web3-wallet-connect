8ef90eae641bb740bae4fe331fa432b3e22d1839
Piotr Leszkowicz <piotr@bayzat.com>
2025-02-18 16:40:41 +0100

    Remove fetchTokens function

--- lib/fetchTokens.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { promises as fs } from 'fs';
-
-export async function fetchTokens() {
-  const filePath = path.join(process.cwd(), 'data', 'tokens.json');
-  const jsonData = await fs.readFile(filePath, 'utf8');
-  return JSON.parse(jsonData);
-}
