e5cd986ee17cd4cfa3d5b77dccb7f6051c26bccd
Piotr Leszkowicz <piotr@bayzat.com>
2025-04-08 15:52:21 +0200

    Add additional notes and links to WalletConnect component for NFT Marketplace guidance

--- components/wallet-connect.tsx
+++ components/wallet-connect.tsx
@@ -2,9 +2,10 @@
 import * as React from 'react';
 import { Connector, useConnect } from 'wagmi';
 import { Button } from '@/components/ui/button';
-import { Loader2 } from 'lucide-react';
+import { InfoIcon, Loader2 } from 'lucide-react';
 import Image from 'next/image';
 import { CardLayout } from './card-layout';
+import Link from 'next/link';
 
 export function WalletConnect() {
   const { connectors, connect, isPending } = useConnect();
@@ -20,6 +21,22 @@ export function WalletConnect() {
             pending={isPending}
           />
         ))}
+      </div>
+      <div className="mt-6 border rounded p-4 text-slate-600">
+        <h2 className="flex items-center">
+          <span className="inline-block h-6 w-6 mr-2">
+            <InfoIcon display="inline" />
+          </span>
+          Additional note:
+        </h2>
+        <h3 className="mt-4">Welcome to the NFT Marketplace!</h3>
+        <p>
+          In order to test the functionality, you have to configure{' '}
+          <Link href="https://github.com/pleszkowicz/hardhat-smart-contract" className="text-blue-800" target="_blank">
+            pleszkowicz/hardhat-smart-contract
+          </Link>.
+        </p>
+        <p>Once you have working hardhat node and deployed smart-contract, follow <Link href="https://github.com/pleszkowicz/web3-wallet-connect/blob/main/README.md" target="_blank">README.MD</Link></p>
       </div>
     </CardLayout>
   );
@@ -51,7 +68,13 @@ function WalletOption({
       onClick={onClick}
     >
       {connector.icon && (
-        <Image src={connector.icon.trimStart().trimEnd()} alt="connector" width={20} height={20} className="w-[20px] h-[20px] mr-2" />
+        <Image
+          src={connector.icon.trimStart().trimEnd()}
+          alt="connector"
+          width={20}
+          height={20}
+          className="w-[20px] h-[20px] mr-2"
+        />
       )}
       <span>{connector.name}</span>
       {!ready && <span className="ml-auto">Unavailable</span>}
