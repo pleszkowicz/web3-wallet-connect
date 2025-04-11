e844a912935de3c49d1727d8112af9b83068b1fe
Pit <leleleszek@gmail.com>
2025-04-11 16:32:07 +0200

    Add Prettier configuration and plugins; update ESLint rules and Tailwind CSS animations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -7,53 +7,48 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: [
-      'next/core-web-vitals',
-      'next/typescript',
-      'eslint-config-next',
-      'prettier',
-    ],
-    ignorePatterns: ['.next/**', 'lib/generated/prisma/**'],
+    extends: ["next/core-web-vitals", "next/typescript", "eslint-config-next", "prettier"],
+    ignorePatterns: [".next/**", "lib/generated/prisma/**"],
     settings: {
       next: {
-        rootDir: '/',
+        rootDir: "/",
       },
     },
     rules: {
-      'react/jsx-key': [
-        'error',
+      "react/jsx-key": [
+        "error",
         {
           warnOnDuplicates: true,
         },
       ],
-      'import/newline-after-import': [
-        'error',
+      "import/newline-after-import": [
+        "error",
         {
           count: 1,
         },
       ],
-      'padding-line-between-statements': [
-        'error',
+      "padding-line-between-statements": [
+        "error",
         {
-          blankLine: 'always',
-          prev: '*',
-          next: ['export'],
+          blankLine: "always",
+          prev: "*",
+          next: ["export"],
         },
         {
-          blankLine: 'never',
-          prev: '*',
-          next: 'import',
+          blankLine: "never",
+          prev: "*",
+          next: "import",
         },
       ],
-      'curly': 'error',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-      ],
-      'eol-last': ['error', 'always'],
-      '@next/next/no-html-link-for-pages': 'off',
-      '@next/next/no-assign-module-variable': 'off',
-      '@typescript-eslint/no-require-imports': 'warn'
+      curly: "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "eol-last": ["error", "always"],
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-assign-module-variable": "off",
+      "@typescript-eslint/no-require-imports": "warn",
+      // 'quotes': ['error', 'double', { avoidEscape: true }],
     },
   }),
 ];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
-				'fade-in': 'fade-in 0.5s ease-out forwards',
+				'fade-in': 'fade-in 2s ease-out forwards',
 				'float': 'float 3s ease-in-out infinite',
 			}
 		}
