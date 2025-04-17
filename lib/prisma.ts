45c9b90a047dd5b3ccd9fe773a637b69e455c9f3
Pit <leleleszek@gmail.com>
2025-04-17 17:50:21 +0200

    Remove unused Prisma extensions and simplify Prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@/lib/generated/prisma"
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { PrismaNeon } from '@prisma/adapter-neon'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
-
-export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+export const prisma =
+    globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
 
-export default prisma
