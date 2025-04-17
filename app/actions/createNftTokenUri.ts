5aa92ac60dabb241f7504faafead0a3d6bce1008
Pit <leleleszek@gmail.com>
2025-04-17 20:00:54 +0200

    Remove temporary test return value from createNftTokenUri function

--- app/actions/createNftTokenUri.ts
+++ app/actions/createNftTokenUri.ts
@@ -5,11 +5,6 @@ const prisma = new PrismaClient();
 
 export async function createNftTokenUri({ name, description, image }: Prisma.NftCreateInput) {
     try {
-        // TODO remove after tests
-        return {
-            abc: 2
-        }
-
         if (!name || !description || !image) {
             return { error: 'All fields are required.' };
         }
