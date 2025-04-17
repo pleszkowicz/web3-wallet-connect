02d421c2085b449fe432878de6f2d6afb6badb8a
Pit <leleleszek@gmail.com>
2025-04-17 19:55:34 +0200

    Refactor NFT token URI handling: update import path and remove unused POST route

--- app/actions/createNftTokenUri.ts
+++ app/actions/createNftTokenUri.ts
@@ -1,10 +1,15 @@
 'use server'
-import { Prisma, PrismaClient } from '@/lib/generated/prisma/edge'
+import { Prisma, PrismaClient } from "@/lib/generated/prisma"
 
 const prisma = new PrismaClient();
 
 export async function createNftTokenUri({ name, description, image }: Prisma.NftCreateInput) {
     try {
+        // TODO remove after tests
+        return {
+            abc: 1
+        }
+
         if (!name || !description || !image) {
             return { error: 'All fields are required.' };
         }
