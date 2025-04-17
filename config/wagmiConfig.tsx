c6f700cb135daa54316564ce7c3eba7d07fa1147
Pit <leleleszek@gmail.com>
2025-04-17 15:07:43 +0200

    Update chains order

--- config/wagmiConfig.tsx
+++ config/wagmiConfig.tsx
@@ -7,21 +7,21 @@ const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string;
 invariant(ALCHEMY_API_KEY, 'ALCHEMY_API_KEY is not found');
 
 export const chainTransportsURLMap = {
-  [mainnet.id]: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
   [sepolia.id]: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  [mainnet.id]: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
 };
 
 export const transports = {
-  [mainnet.id]: http(chainTransportsURLMap[mainnet.id]),
   [sepolia.id]: http(chainTransportsURLMap[sepolia.id]),
   [hardhat.id]: http(), // created custom transaction history handler
+  [mainnet.id]: http(chainTransportsURLMap[mainnet.id]),
 };
 
 export const config = createConfig({
   chains: [
-    mainnet,
     sepolia, // testnet
     hardhat,
+    mainnet,
   ],
   connectors: [],
   transports,
