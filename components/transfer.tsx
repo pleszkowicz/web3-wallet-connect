8483362f6d60d6b612b0086af91c1615a1bc2f76
Pit <leleleszek@gmail.com>
2025-04-24 11:36:11 +0200

    Refactor Transfer component: replace useGasPrice with custom gas price fetching logic and enhance gas fee display

--- components/transfer.tsx
+++ components/transfer.tsx
@@ -6,9 +6,9 @@ import { SEPOLIA_LINK_CONTRACT_ADDRESS, SEPOLIA_LINK_TOKEN_ABI } from '@/const/s
 import { cn } from '@/lib/cn';
 import { ErrorMessage, Field, Form, Formik, useFormikContext } from 'formik';
 import { RefreshCwIcon } from 'lucide-react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Address, formatEther, isAddress, parseEther } from 'viem';
-import { useAccount, useBalance, useGasPrice, useReadContract, useSendTransaction, useWriteContract } from 'wagmi';
+import { useAccount, useBalance, usePublicClient, useReadContract, useSendTransaction, useWriteContract } from 'wagmi';
 import * as Yup from 'yup';
 import { CardLayout } from './CardLayout';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
@@ -47,7 +47,24 @@ export function Transfer() {
   });
 
   const { writeContract, isPending: isWriteContractPending } = useWriteContract();
-  const { data: gasPrice, isFetching: isGasPriceFetching, refetch: refetchGasPrice } = useGasPrice();
+
+  const client = usePublicClient();
+  const [gasPrice, setGasPrice] = useState<bigint | undefined>();
+  const [isGasPriceFetching, setIsGasPriceFetching] = useState(false);
+
+  const fetchGas = useCallback(async () => {
+    try {
+      setIsGasPriceFetching(true);
+      const gasPrice = await client?.estimateMaxPriorityFeePerGas();
+      setGasPrice(gasPrice);
+    } finally {
+      setIsGasPriceFetching(false);
+    }
+  }, [client]);
+
+  useEffect(() => {
+    fetchGas();
+  }, [fetchGas]);
 
   const currentBalance = (selectedUnit === CryptoMap.ETH ? ethBalance?.value : (linkBalance as bigint)) ?? 0;
 
@@ -99,58 +116,73 @@ export function Transfer() {
         }}
         validationSchema={validationSchema}
       >
-        <Form className="space-y-4">
-          <div className="space-y-2">
-            <CryptoSelect
-              name="unit"
-              onChange={(unitValue) => setSelectedUnit(CryptoMap[unitValue.toUpperCase() as CryptoMapKey])}
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="from">From</Label>
-            <Field as={Input} id="from" name="from" type="text" disabled className="bg-muted" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="to">To</Label>
-            <Field as={Input} id="to" name="to" placeholder="0x..." />
-            <ErrorMessage name="to" component="div" className="text-red-500" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="value">
-              Value{' '}
-              <span className="text-xs">
-                (max {currentBalance ? formatEther(currentBalance as bigint) : '0.00'}{' '}
-                {selectedUnit.value.toUpperCase()})
-              </span>
-            </Label>
-            <Field as={Input} id="value" name="value" placeholder="0.00" />
-            <ErrorMessage name="value" component="div" className="text-red-500" />
-          </div>
-          <div className={cn('space-y-2', isGasPriceFetching ? 'animate-pulse disabled' : '')}>
-            <Label>Estimated Gas Fee</Label>
-            <div className="flex items-center justify-start gap-2 bg-muted p-2 rounded-md">
-              <span className="text-sm">{gasPrice ? `${formatEther(gasPrice, 'gwei')} Gwei` : 'Loading...'}</span>{' '}
-              {gasPrice ? <span className="text-xs">({formatEther(gasPrice, 'wei')} ETH)</span> : null}
+        {({ values }) => {
+          return (
+            <Form className="space-y-4">
+              <div className="space-y-2">
+                <CryptoSelect
+                  name="unit"
+                  onChange={(unitValue) => setSelectedUnit(CryptoMap[unitValue.toUpperCase() as CryptoMapKey])}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="from">From</Label>
+                <Field as={Input} id="from" name="from" type="text" disabled className="bg-muted" />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="to">To</Label>
+                <Field as={Input} id="to" name="to" placeholder="0x..." />
+                <ErrorMessage name="to" component="div" className="text-red-500" />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="value">
+                  Value{' '}
+                  <span className="text-xs">
+                    (max {currentBalance ? formatEther(currentBalance as bigint) : '0.00'}{' '}
+                    {selectedUnit.value.toUpperCase()})
+                  </span>
+                </Label>
+                <Field as={Input} id="value" name="value" placeholder="0.00" />
+                <ErrorMessage name="value" component="div" className="text-red-500" />
+              </div>
+              <div className={cn('space-y-2', isGasPriceFetching ? 'animate-pulse disabled' : '')}>
+                <div className="flex items-center justify-start gap-2 bg-muted p-2 rounded-md">
+                  <Label>Estimated Gas Fee:</Label>
+                  <span className="text-sm">
+                    {gasPrice ? `${formatEther(gasPrice, 'gwei')} Gwei` : 'Loading...'}
+                  </span>{' '}
+                  <Button
+                    type="button"
+                    variant="link"
+                    size="sm"
+                    disabled={isGasPriceFetching}
+                    onClick={() => !isGasPriceFetching && fetchGas()}
+                  >
+                    <RefreshCwIcon className={cn('w-4 h-4 mr-2', isGasPriceFetching ? 'animate-spin' : '')} />
+                  </Button>
+                  {/* {gasPrice ? (
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <InfoIcon className="inline-block" width={20} />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <span className="text-xs">({formatEther(gasPrice, 'wei')} ETH)</span>
+                      </TooltipContent>
+                    </Tooltip>
+                  ) : null} */}
+                </div>
+              </div>
               <Button
-                type="button"
-                variant="link"
-                size="sm"
-                disabled={isGasPriceFetching}
-                onClick={() => !isGasPriceFetching && refetchGasPrice()}
+                disabled={isTransactionPending || isWriteContractPending}
+                variant="default"
+                className="w-full"
+                type="submit"
               >
-                <RefreshCwIcon className={cn('w-4 h-4 mr-2', isGasPriceFetching ? 'animate-spin' : '')} />
+                Transfer
               </Button>
-            </div>
-          </div>
-          <Button
-            disabled={isTransactionPending || isWriteContractPending}
-            variant="default"
-            className="w-full"
-            type="submit"
-          >
-            Transfer
-          </Button>
-        </Form>
+            </Form>
+          );
+        }}
       </Formik>
     </CardLayout>
   );
