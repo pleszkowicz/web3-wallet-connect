883d65152dcaaa993139e41d1ee255e8a999ce8f
Pit <leleleszek@gmail.com>
2025-04-25 13:52:41 +0200

    Remove .env file and update wagmiConfig to exclude mainnet configuration

--- config/wagmiConfig.tsx
+++ config/wagmiConfig.tsx
@@ -1,6 +1,6 @@
 import invariant from 'tiny-invariant';
 import { createConfig, webSocket } from 'wagmi';
-import { hardhat, mainnet, sepolia } from 'wagmi/chains';
+import { hardhat, sepolia } from 'wagmi/chains';
 
 const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!;
 invariant(ALCHEMY_API_KEY, 'ALCHEMY_API_KEY is not found');
@@ -10,10 +10,6 @@ export const chainTransportsURLMap = {
     webSocket: `wss://eth-sepolia.g.alchemy.com/v2/P_GFW7qc_w2oYJmtO7PYYtdPwNESKN96`,
     http: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
   },
-  [mainnet.id]: {
-    webSocket: `wss://eth-mainnet.g.alchemy.com/v2/P_GFW7qc_w2oYJmtO7PYYtdPwNESKN96`,
-    http: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-  },
   [hardhat.id]: {
     webSocket: 'ws://localhost:8545',
     http: undefined,
@@ -22,12 +18,11 @@ export const chainTransportsURLMap = {
 
 export const transports = {
   [sepolia.id]: webSocket(chainTransportsURLMap[sepolia.id].webSocket, { retryCount: 0 }),
-  [mainnet.id]: webSocket(chainTransportsURLMap[mainnet.id].webSocket, { retryCount: 0 }),
   [hardhat.id]: webSocket(chainTransportsURLMap[hardhat.id].webSocket, { retryCount: 0 }),
 };
 
 export const config = createConfig({
-  chains: [sepolia, mainnet, hardhat],
+  chains: [sepolia, hardhat],
   connectors: [],
   transports,
 });
