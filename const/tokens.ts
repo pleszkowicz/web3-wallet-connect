a42f83e8fd572ca289a9ddb15c48e01ddeaf8678
Pit <leleleszek@gmail.com>
2025-05-19 20:10:55 +0200

    feat: add DAI token ABI and update token map with DAI and WBTC details

--- const/tokens.ts
+++ const/tokens.ts
@@ -1,9 +1,8 @@
 import { Abi, Address } from 'viem';
-import { WETH_ABI } from './abi/weth-abi';
+import { DAI_TOKEN_ABI } from './abi/dai-token-abi';
 import { LINK_TOKEN_ABI } from './abi/link-token-abi';
-import { DOG_TOKEN_ABI } from './abi/dog-token-abi';
 import { USDC_ABI } from './abi/usdc-abi';
-import { ANYMAL_PROTOCOL_ABI } from './abi/anymal-protocol-token-abi';
+import { WETH_ABI } from './abi/weth-abi';
 
 export type Token = {
   symbol: string;
@@ -11,38 +10,60 @@ export type Token = {
   decimals: number;
   label: string;
   abi?: Abi;
+  logo: string;
 };
 
 export type TokenMapKey = keyof typeof tokenMap;
 
 export const tokenMap = {
   eth: {
-    symbol: 'eth', label: 'Ethereum (ETH)', address: undefined, decimals: 18, abi: undefined,
+    symbol: 'eth',
+    label: 'SepoliaETH',
+    address: undefined,
+    decimals: 18,
+    abi: undefined,
+    logo: 'https://token-icons.s3.amazonaws.com/eth.png',
   },
   weth: {
     symbol: 'weth',
-    label: 'Wrapped Ethereum (WETH)',
-    address: '0x4200000000000000000000000000000000000006',
+    label: 'Wrapped Ethereum',
+    address: '0x7b79995e5f793a07bc00c21412e50ecae098e7f9',
     decimals: 18,
     abi: WETH_ABI,
+    logo: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
   },
-  usdc: { symbol: 'usdc', label: 'USD Coin (USDC)', address: '0x036CbD53842c5426634e7929541eC2318f3dCF7e', decimals: 6, abi: USDC_ABI },
+  usdc: {
+    symbol: 'usdc',
+    label: 'USD Coin',
+    address: '0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8',
+    decimals: 6,
+    abi: USDC_ABI,
+    logo: 'https://coin-images.coingecko.com/coins/images/6319/large/usdc.png',
+  },
+  dai: {
+    symbol: 'dai',
+    label: 'Dai Stablecoin',
+    address: '0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357',
+    decimals: 18,
+    abi: DAI_TOKEN_ABI,
+    logo: 'https://coin-images.coingecko.com/coins/images/9956/large/Badge_Dai.png'
+  },
   link: {
     symbol: 'link',
-    label: 'ChainLink Token (LINK)',
-    address: '0xE4aB69C077896252FAFBD49EFD26B5D171A32410',
+    label: 'ChainLink Token',
+    address: '0x779877A7B0D9E8603169DdbD7836e478b4624789',
     decimals: 18,
     abi: LINK_TOKEN_ABI,
+    logo: 'https://coin-images.coingecko.com/coins/images/877/large/chainlink-new-logo.png',
   },
-  dog: { symbol: 'dog', label: 'DogToken (DOG)', address: '0x41e7D8eD48b01138B7532D70b1edCb551df8c9d6', decimals: 18, abi: DOG_TOKEN_ABI },
-  anymal: {
-    symbol: 'anymal',
-    label: 'Anymal Protocol (ANYMAL)',
-    address: '0xfb65eF78e5B8D7718cf9BebAdfcCF3E46bD846Ac',
-    decimals: 18,
-    abi: ANYMAL_PROTOCOL_ABI,
+  wbtc: {
+    symbol: 'wbtc',
+    label: 'Wrapped BTC',
+    address: '0x21472df1b5d2b673f6444b41258c6460294a2c06',
+    decimals: 8,
+    abi: undefined,
+    logo: 'https://coin-images.coingecko.com/coins/images/7598/large/wrapped_bitcoin_wbtc.png?1696507857'
   },
 } as const;
-
 
 export const tokens: Token[] = Object.values(tokenMap);
