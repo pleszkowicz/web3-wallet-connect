1f1e3c6ec6b52d5d003c36daaf69804693b958cd
Pit <leleleszek@gmail.com>
2025-05-19 20:10:15 +0200

    Refactor wagmiConfig: Remove base and baseSepolia configurations for cleaner setup

--- config/wagmiConfig.tsx
+++ config/wagmiConfig.tsx
@@ -1,18 +1,18 @@
 import invariant from 'tiny-invariant';
 import { createConfig, webSocket } from 'wagmi';
-import { base, baseSepolia, hardhat } from 'wagmi/chains';
+import { hardhat, sepolia } from 'wagmi/chains';
 
 const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 invariant(ALCHEMY_API_KEY, 'ALCHEMY_API_KEY is not found');
 
 export const chainTransportsURLMap = {
-  [base.id]: {
-    webSocket: `wss://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-    http: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-  },
-  [baseSepolia.id]: {
-    webSocket: `wss://base-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-    http: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  // [base.id]: {
+  //   webSocket: `wss://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  //   http: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  // },
+  [sepolia.id]: {
+    webSocket: `wss://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    http: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
   },
   [hardhat.id]: {
     webSocket: 'ws://localhost:8545',
@@ -21,13 +21,13 @@ export const chainTransportsURLMap = {
 };
 
 export const transports = {
-  [base.id]: webSocket(chainTransportsURLMap[base.id].webSocket, { retryCount: 0 }),
-  [baseSepolia.id]: webSocket(chainTransportsURLMap[baseSepolia.id].webSocket, { retryCount: 0 }),
+  // [base.id]: webSocket(chainTransportsURLMap[base.id].webSocket, { retryCount: 0 }),
+  [sepolia.id]: webSocket(chainTransportsURLMap[sepolia.id].webSocket, { retryCount: 0 }),
   [hardhat.id]: webSocket(chainTransportsURLMap[hardhat.id].webSocket, { retryCount: 0 }),
 };
 
 export const config = createConfig({
-  chains: [base, baseSepolia, hardhat],
+  chains: [/*base, */ sepolia, hardhat],
   connectors: [],
   ssr: true,
   transports,
