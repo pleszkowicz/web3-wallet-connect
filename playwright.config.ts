da0e1766fc95d8a88eff47a490c1ca0593d8ae7e
Pit <leleleszek@gmail.com>
2025-06-04 16:35:28 +0200

    Refactor Playwright configuration: update server command to 'pnpm run dev' for consistency in local development

--- playwright.config.ts
+++ playwright.config.ts
@@ -82,7 +82,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run start',
+    command: 'pnpm run dev',
     url: 'http://localhost:3000',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI, // Reuse existing server in non-CI environments
