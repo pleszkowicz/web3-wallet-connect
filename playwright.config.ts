1d5f3d2f20f8869cad7fc730e88c7c30374a9aea
Pit <leleleszek@gmail.com>
2025-06-04 16:42:26 +0200

    Refactor Playwright configuration: set headless mode to true for consistent test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
     trace: process.env.CI ? 'retain-on-first-failure' : 'on',
-    headless: false,
+    headless: true,
 
   },
   maxFailures: process.env.CI ? 0 : 1,
