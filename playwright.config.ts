e1642eab16d33d2ed996ef1b5643c5a39409087f
Pit <leleleszek@gmail.com>
2025-06-05 23:10:53 +0200

    Update frontend build command in E2E workflow and change web server command in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -83,7 +83,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run dev',
+    command: 'pnpm run start',
     url: 'http://localhost:3000',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI, // Reuse existing server in non-CI environments
